bfd290a1225654a399c9fbfdc1305546a1c00b49
enkhbilguutei <enkhbilguutei01@gmail.com>
2022-04-19 20:33:25 +0800

    about us

--- components/about-us-trust-section/index.tsx
+++ components/about-us-trust-section/index.tsx
@@ -1,11 +1,12 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid } from "@mui/material";
+import { Typography } from "@mui/material";
 
 export default function TrustSection() {
   return (
     <Container>
       <Grid container>
         <Grid item xs={5}>
-          <Typography variant="h4" align="center" sx={{ fontWeight: "bold" }}>
+          <Typography variant="h4" align="center">
             Яагаад биднийг сонгох вэ?
           </Typography>
         </Grid>
